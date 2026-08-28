f98bb93f03ac00e00beec60b530ca918ad8a648a
kevpot404 <potkev@proton.me>
2026-08-29 00:07:49 +0300

    start model data preprocessing

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,8 +1,24 @@
-from dotenv import load_dotenv
-import os
+def preprocess_features(df):
+    """
+    Preprocess the dataset by removing unwanted features.
+    Decisions made through looking at ./data/dataset_audit.ipynb.
+    """
 
-load_dotenv()
+    columns_to_drop = [
+        "nox",
+        "no",
+        "co",
+        "so2",
+        "sunset",
+        "sunrise",
+        "player_team",
+        "event_name",
+        "location_city",
+        "opponent",
+        "location_openaq",
+        "weather_code"
+    ]
 
-api_key = os.getenv("API_KEY")
+    df = df.drop(columns=columns_to_drop, errors="ignore")
 
-
+    return df
